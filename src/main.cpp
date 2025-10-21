643190c9bfcc0230e062d2c87e3776f755ccda4f
Pavel Rozhkov <mrkraft500@gmail.com>
2025-10-22 01:19:20 +0300

    NEW: resources: add first picture

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,12 @@
 
 int main ()
 {
-    setenv("QT_LOGGING_RULES", "*=false", 1);
-    cv::namedWindow ("OpenCV test", cv::WINDOW_AUTOSIZE);
+  cv::namedWindow ("OpenCV test", cv::WINDOW_AUTOSIZE);
 
-    cv::Mat image = cv::Mat::zeros(480, 640, CV_8UC3);
-    image.setTo (cv::Scalar (255, 0, 0));
+  cv::Mat image = cv::imread ("../resources/taiko_load.jpg", 1);
 
-    cv::putText (image, "Test Image", cv::Point (50, 50), cv::FONT_HERSHEY_SIMPLEX,
-            1, cv::Scalar (0, 255, 0), 2);
+  cv::imshow ("OpenCV test", image);
 
-    cv::imshow ("OpenCV test", image);
-
-    cv::waitKey (0);
-    cv::destroyAllWindows ();
+  cv::waitKey (0);
+  cv::destroyAllWindows ();
 }
