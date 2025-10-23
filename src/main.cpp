437f0ac36d3c9cd9abbe371f808a161aff8d8108
pavel.rozhkov <mrkraft500@gmail.com>
2025-10-24 00:44:26 +0300

    REF: main: use main_loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,31 @@
 
 #include "globals.h"
 
-#include "system/include/screen.h"
+// #include "system/include/screen.h"
+
+#include "game/include/game_loop.h"
 
 int main ()
 {
-  cv::namedWindow (WINDOW_NAME, cv::WINDOW_NORMAL);
-  cv::setWindowProperty (WINDOW_NAME, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+  // cv::namedWindow (WINDOW_NAME, cv::WINDOW_NORMAL);
+  // cv::setWindowProperty (WINDOW_NAME, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
 
-  system_utils::screen s;
+  // system_utils::screen s;
 
-  const int screen_width = s.get_width ();
-  const int screen_height = s.get_height (); 
+  // const int screen_width = s.get_width ();
+  // const int screen_height = s.get_height (); 
   
-  cv::Mat original_image = cv::imread ("../resources/taiko_load.jpg", 1);
-  cv::Mat resized_image;
+  // cv::Mat original_image = cv::imread ("../resources/taiko_load.jpg", 1);
+  // cv::Mat resized_image;
 
-  cv::Size screen_size (screen_width, screen_height);
-  cv::resize (original_image, resized_image, screen_size, 0, 0, cv::INTER_LINEAR);
+  // cv::Size screen_size (screen_width, screen_height);
+  // cv::resize (original_image, resized_image, screen_size, 0, 0, cv::INTER_LINEAR);
 
-  cv::imshow (WINDOW_NAME, resized_image);
+  // cv::imshow (WINDOW_NAME, resized_image);
 
-  cv::waitKey (0);
-  cv::destroyAllWindows ();
+  // cv::waitKey (0);
+  // cv::destroyAllWindows ();
+
+  game::main_loop app;
+  return app.run ();
 }
