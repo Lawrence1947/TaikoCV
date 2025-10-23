3de8571bdbd2635ab15bf5a453445ce0805984ae
pavel.rozhkov <mrkraft500@gmail.com>
2025-10-24 01:59:55 +0300

    ENH: kernel: timer realization

--- src/kernel/include/timer.h
+++ src/kernel/include/timer.h
@@ -1,16 +1,32 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <chrono>
+
 namespace kernel
 {
 
 class timer
 {
+  using clock = std::chrono::high_resolution_clock;
+  using duration = std::chrono::duration<float>;
+
 public:
   timer ();
 
+  void tick ();
+
+  float delta   () const { return delta_t; }
+  float elapsed () const { return elapsed_t; }
+  float fps     () const { return 1.0f / delta_t; }
 
   ~timer ();
+
+private:
+  float target_delta_t;
+  float delta_t;
+  float elapsed_t;
+  clock::time_point prev_t;
 };
 
 }
--- src/kernel/src/timer.cpp
+++ src/kernel/src/timer.cpp
@@ -1,17 +1,38 @@
 #include "timer.h"
 
+#include <thread>
+
+#include "globals.h"
 
 namespace kernel
 {
 
-timer::timer ()
+timer::timer () : 
+  target_delta_t (1.f / target_fps),
+  elapsed_t (0.f),
+  delta_t (0.f),
+  prev_t (clock::now ()) {}
+
+void timer::tick ()
 {
-  
+  auto curr_t = clock::now ();
+  auto elapsed = curr_t - prev_t;
+  float frame_delta_t = std::chrono::duration_cast<duration> (elapsed).count ();
+
+
+  if (frame_delta_t < target_delta_t)
+    {
+      auto sleep_duration = duration (target_delta_t - frame_delta_t);
+      std::this_thread::sleep_for (std::chrono::duration_cast<std::chrono::microseconds> (sleep_duration));
+      curr_t = clock::now ();
+      frame_delta_t = duration (target_delta_t - frame_delta_t).count ();
+    }
+
+  delta_t = frame_delta_t;
+  elapsed_t += frame_delta_t;
+  prev_t = curr_t;
 }
 
-timer::~timer ()
-{
-  
-}
+timer::~timer () {}
 
 }
