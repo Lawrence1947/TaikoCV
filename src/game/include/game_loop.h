00a49fea7b224c2234e0d0aea6e87a91b9426aaf
pavel.rozhkov <mrkraft500@gmail.com>
2025-10-24 00:38:30 +0300

    REF: game: add initial game_loop definition

---
+++ src/game/include/game_loop.h
@@ -0,0 +1,17 @@
+
+
+namespace game
+{
+
+class main_loop
+{
+public:
+
+  main_loop ();
+
+  int run ();
+
+  ~main_loop ();
+};
+
+}
---
+++ src/game/src/game_loop.cpp
@@ -0,0 +1,23 @@
+#include "game_loop.h"
+
+
+namespace game
+{
+
+main_loop::main_loop ()
+{
+
+}
+
+int run ()
+{
+  return 0;
+}
+
+main_loop::~main_loop ()
+{
+
+}
+
+  
+}
