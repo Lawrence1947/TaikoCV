93786863bb13725a39fe8528697f227f1b13a7d4
pavel.rozhkov <mrkraft500@gmail.com>
2025-10-25 00:07:47 +0300

    ENH: menu: add text to main menu

--- src/game/src/menu.cpp
+++ src/game/src/menu.cpp
@@ -1,6 +1,7 @@
 #include "menu.h"
 
 #include <stdio.h>
+#include <string>
 
 namespace game
 {
@@ -10,22 +11,31 @@ menu::menu (const cv::Size &screen_size_) : screen_size (screen_size_)
   original_data.main_screen = cv::imread ("../resources/main_menu/taiko_main_screen.jpg", cv::IMREAD_UNCHANGED);
 
   cv::Rect fullscreen (0, 0, screen_size.width, screen_size.height);
+  cv::Mat frame = original_data.main_screen;
 
-  kernel::object main_screen (original_data.main_screen, fullscreen);
+  std::string text = "Press ENTER to start";
+  constexpr double scale = 1.4;
+  constexpr int thickness = 4;
+  cv::Scalar color (0, 0, 255);
+  int font = cv::FONT_HERSHEY_TRIPLEX | cv::FONT_ITALIC;
 
+  cv::Size textSize = cv::getTextSize(text, font, scale, thickness, nullptr);
+  int x = (screen_size.width - textSize.width) / 2;
+  int y = screen_size.height * 0.85;
+
+  cv::putText (frame, text, {x + 2, y + 2}, font, scale, cv::Scalar (0, 0, 0), thickness + 2, cv::LINE_AA);
+
+  cv::putText (frame, text, {x, y}, font, scale, color, thickness, cv::LINE_AA);
+
+  kernel::object main_screen (frame, fullscreen);
   objects.push_back (main_screen);
 }
 
 void menu::update (const float delta_t)
 {
   printf ("main menu: %f\n", delta_t);
-
-  
 }
 
-menu::~menu () 
-{
-
-}
+menu::~menu () {}
 
 }
